8aca0c8ae68ec78687d52da709c2311338c2cc2d
Chiahao Lin <y80811@gmail.com>
2015-11-12 02:02:20 +0800

    add create, delete and read method

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ async function setup() {
     conn = await r.connect(config);
     // create table authors
     // let result = await r.db('test').tableCreate('authors').run(conn);
-    // console.log(JSON.stringify(result));
+    // console.log(result);
     // start web server
     app.listen(3000);
   } catch(err) {
@@ -23,13 +23,15 @@ async function setup() {
 
 }
 
-// query all records in authors tables
+// get records from authors table by author
 app.use(function* (next) {
-  if(this.path !== '/' || this.method !== 'GET') return yield next;
+  if(this.path !== '/authors' || this.method !== 'GET') return yield next;
   try {
-    let cursor = yield r.table('authors').run(conn);
+    let cursor = null;
+    if(this.query.author) cursor = yield r.table('authors').filter({author: this.query.author}).run(conn);
+    else cursor = yield r.table('authors').run(conn);
     let result = yield cursor.toArray();
-    this.body = JSON.stringify(result);
+    this.body = result;
   } catch(err) {
     console.error(err.stack);
     this.response.status = 500;
@@ -39,17 +41,32 @@ app.use(function* (next) {
 
 // insert a record to authors table
 app.use(function* (next) {
-  if(this.path !== '/authors' || this.method !== 'POST') return yield next;
+  if(this.path !== '/authors/create' || this.method !== 'POST') return yield next;
   try {
     let body = yield parse(this, {limit: '1kb'});
     let result = yield r.table('authors').insert([body]).run(conn);
-    this.body = JSON.stringify(result);
+    this.body = result;
   } catch(err) {
     console.error(err.stack);
     this.response.status = 500;
     return yield next;
   }
 });
+
+// delete a record from authors table by author
+app.use(function* (next) {
+  if(this.path !== '/authors/delete' || this.method !== 'DELETE') return yield next;
+  try {
+    let body = yield parse(this, {limit: '1kb'});
+    let result = yield r.table('authors').filter({author: body.author}).delete().run(conn);
+    this.body = result;
+  } catch(err) {
+    console.error(err.stack);
+    this.response.status = 500;
+    return yield next;
+  }
+});
+
 
 // run
 setup()
